c28b66aaebe4872676091c86e3bf60475f980ca3
nattawara <nattawara.jtk@gmail.com>
2024-05-22 15:13:06 +0700

    ploy/JavaScript Skill Checkpoint #1

--- question-3.js
+++ question-3.js
@@ -1,3 +1,13 @@
 // Question #3
 let userPassword = "";
 // เริ่มเขียนโค้ดตรงนี้
+function checkPasswordStrength(userPassword) {
+  if (userPassword.length < 6) {
+    return "Weak";
+  } else if (userPassword.length >= 6 && userPassword.length <= 10) {
+    return "Medium";
+  } else {
+    return "strong";
+  }
+}
+console.log(checkPasswordStrength(userPassword));
--- question-5.js
+++ question-5.js
@@ -19,3 +19,21 @@ const products = [
 
 const promotionCode = "";
 // เริ่มเขียนโค้ดตรงนี้
+const calculateTotalPrice = function (product, code) {
+  //console.log(product);
+  const totalBeforeDiscount = product
+    .map((item) => item.price * item.quantity)
+    .reduce((acc, curr) => acc + curr, 0);
+  //console.log(totalBeforeDiscount);
+
+  let discount = 1;
+  if (code === "SALE20") {
+    discount -= 0.2;
+  } else if (code === "SALE50") {
+    discount -= 0.5;
+  }
+  //console.log(discount);
+  return totalBeforeDiscount * discount;
+};
+
+console.log(calculateTotalPrice(products, promotionCode));
